8960564100171806e019822fda67db8388b21268
Jae Han Kim <hgal8877@gmail.com>
2019-10-28 17:30:17 +0900

    commit security config

---
+++ scinemapr.bo/src/main/java/com/eascapeco/scinemapr/bo/security/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.eascapeco.scinemapr.bo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.httpBasic().disable()
+			.csrf().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+				.authorizeRequests()
+					.antMatchers("/loginForm").permitAll()
+					.anyRequest().hasRole("USER");
+		super.configure(http);
+	}
+	
+	@Override
+	public void init(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/static/**");
+		super.init(web);
+	}
+	
+}
