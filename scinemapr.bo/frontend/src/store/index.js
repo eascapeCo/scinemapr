7bca57932d7f7c4d5715f784e71a68d5ff709ed3
Jaeyoung You <ruew12@gmail.com>
2020-06-10 17:52:19 +0900

    store commit

--- scinemapr.bo/frontend/src/store/index.js
+++ scinemapr.bo/frontend/src/store/index.js
@@ -1,14 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    accessToken: null,
+    refreshToken: null
+  },
+  getters: {
+    getJwt: function (state) {
+      return state.accessToken
+    }
   },
   mutations: {
+    LOGIN_SUCCESS: (state, headers) => {
+      console.log('muta -> ' + JSON.stringify(headers))
+      document.cookie = 'refreshToken=' + headers.tokenList.refreshToken
+      state.accessToken = headers.tokenList.accessToken
+    }
   },
   actions: {
+    login: ({ commit }, { id, pwd }) => {
+      console.log('id -> ' + id + 'pwd -> ' + pwd)
+      return axios.post('/api/admin/login', { id, pwd })
+        .then((res) => res)
+        .catch((error) => error)
+    }
   },
   modules: {
   }
