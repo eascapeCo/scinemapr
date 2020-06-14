ed35b5cdf662a957c8c16ad6dd0af019afe4f06e
JaeHan-Kim <hgal8877@gmail.com>
2020-06-14 10:17:48 +0900

    [임시] 메뉴기능 security 체크 제외

--- scinemapr.bo/src/main/java/com/eascapeco/scinemapr/bo/security/SecurityConfig.java
+++ scinemapr.bo/src/main/java/com/eascapeco/scinemapr/bo/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
 				.authorizeRequests()
-					.antMatchers("/loginForm", "/api/admin/login", "/favicon.ico", "/api/menus").permitAll()
+					.antMatchers("/loginForm", "/api/admin/login", "/favicon.ico", "/api/menus", "/api/menu").permitAll()
 					.anyRequest().hasRole("USER")
 			.and()
 				.exceptionHandling().authenticationEntryPoint(new RestAuthenticationEntryPoint())
