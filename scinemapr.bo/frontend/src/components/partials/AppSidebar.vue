74b23b01b78a685a832c45feb55049e2cda2ae95
JaeHan-Kim <hgal8877@gmail.com>
2020-06-15 22:58:16 +0900

    메뉴 목록 노출 완성

--- scinemapr.bo/frontend/src/components/partials/AppSidebar.vue
+++ scinemapr.bo/frontend/src/components/partials/AppSidebar.vue
@@ -18,99 +18,37 @@
           <v-list-item-title v-text="menu.mnuName"></v-list-item-title>
         </v-list-item-content>
       </template>
-      <!--
-      <v-list-group
-        no-action
-        sub-group
+      <template
         v-for="secondMenu in menu.subMenus"
-        :key="secondMenu.mnuName"
       >
-        <template v-slot:activator>
-          <v-list-item-content>
-            <v-list-item-title v-text="secondMenu.mnuName"></v-list-item-title>
-          </v-list-item-content>
-        </template>
-        <v-list-item
-          link
-          v-for="thirdMenu in secondMenu.subMenus"
-          :key="thirdMenu.mnuName"
-        >
-          <v-list-item-title v-text="thirdMenu.mnuName"></v-list-item-title>
-        </v-list-item>
-      </v-list-group>
-      -->
-        <span
-          v-for="secondMenu in menu.subMenus"
-          :key="secondMenu.mnuName"
-        >
-          <template v-if="secondMenu.subMenus.length !== 0">
-            <v-list-group
-              no-action
-              sub-group
-            >
-              <template v-slot:activator>
-                <v-list-item-content>
-                  <v-list-item-title v-text="secondMenu.mnuName"></v-list-item-title>
-                </v-list-item-content>
-              </template>
-              <v-list-item
-                link
-                v-for="thirdMenu in secondMenu.subMenus"
-                :key="thirdMenu.mnuName"
-              >
-                <v-list-item-title v-text="thirdMenu.mnuName"></v-list-item-title>
-              </v-list-item>
-            </v-list-group>
-          </template>
-          <template v-if="secondMenu.subMenus.length === 0">
-            <v-list-item link>
+        <template v-if="secondMenu.subMenus.length !== 0">
+          <v-list-group
+            no-action
+            sub-group
+            v-bind:key="secondMenu.mnuName"
+          >
+            <template v-slot:activator>
               <v-list-item-content>
                 <v-list-item-title v-text="secondMenu.mnuName"></v-list-item-title>
               </v-list-item-content>
+            </template>
+            <v-list-item
+              link
+              v-for="thirdMenu in secondMenu.subMenus"
+              :key="thirdMenu.mnuName"
+            >
+              <v-list-item-title v-text="thirdMenu.mnuName"></v-list-item-title>
             </v-list-item>
-          </template>
-        </span>
-      <v-list-item link>
-        <v-list-item-content>
-          <v-list-item-title @click="test">Ao3 - 3</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list-group>
-    <v-list-group
-        prepend-icon="mdi-view-dashboard"
-        no-action
-    >
-        <template v-slot:activator>
-        <v-list-item-content>
-            <v-list-item-title>Ao3</v-list-item-title>
-        </v-list-item-content>
+          </v-list-group>
         </template>
-        <v-list-group
-        no-action
-        sub-group
-        >
-        <template v-slot:activator>
+        <template v-else>
+          <v-list-item link v-bind:key="secondMenu.mnuName">
             <v-list-item-content>
-            <v-list-item-title>Ao3 - 1</v-list-item-title>
+              <v-list-item-title v-text="secondMenu.mnuName"></v-list-item-title>
             </v-list-item-content>
+          </v-list-item>
         </template>
-        <v-list-item link>
-            <v-list-item-title v-text="123"></v-list-item-title>
-        </v-list-item>
-        <v-list-item link>
-            <v-list-item-title v-text="456"></v-list-item-title>
-        </v-list-item>
-        </v-list-group>
-        <v-list-item link>
-        <v-list-item-content>
-            <v-list-item-title @click="test">Ao3 - 2</v-list-item-title>
-        </v-list-item-content>
-        </v-list-item>
-        <v-list-item link>
-          <v-list-item-content>
-            <v-list-item-title @click="test">Ao3 - 3</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+      </template>
     </v-list-group>
     <v-list-group
       prepend-icon="mdi-view-dashboard"
