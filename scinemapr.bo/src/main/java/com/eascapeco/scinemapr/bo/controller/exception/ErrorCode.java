f454c8d59c0c03a87e02c06f97bc6906d139a445
JaeHan-Kim <hgal8877@gmail.com>
2020-09-06 15:33:29 +0900

    errorcode source update

--- scinemapr.bo/src/main/java/com/eascapeco/scinemapr/bo/controller/exception/ErrorCode.java
+++ scinemapr.bo/src/main/java/com/eascapeco/scinemapr/bo/controller/exception/ErrorCode.java
@@ -1,6 +1,31 @@
 package com.eascapeco.scinemapr.bo.controller.exception;
 
-public enum E {
+public enum ErrorCode {
     NOT_NULL("ERROR_CODE_0001","필수값이 누락되었습니다")
     , MIN_VALUE("ERROR_CODE_0002", "최소값보다 커야 합니다.")
+    , MAX_VALUE("ERROR_CODE_0002", "최댁값보다 작아야 합니다.");
+
+    private String code;
+    private String description;
+
+    ErrorCode(String code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
